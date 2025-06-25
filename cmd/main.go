c02a18a776a65514a76c788263f2f76edd3a228d
Jesse <w_kun@outlook.com>
2025-06-25 14:47:05 +0800

    添加 go-resty 依赖，重构配置加载逻辑，优化服务器启动和优雅退出处理

--- cmd/main.go
+++ cmd/main.go
@@ -17,52 +17,78 @@ import (
 	"github.com/jessewkun/gocommon/logger"
 
 	_ "github.com/jessewkun/gocommon/debug"
+	_ "github.com/jessewkun/gocommon/http"
 )
 
-var configFile string
-var baseConfig *config.BaseConfig
+var (
+	configFile string
+	baseConfig *config.BaseConfig
+)
 
 func init() {
 	flag.StringVar(&configFile, "c", "config.yml", "config file path")
 	flag.Parse()
+}
 
+func loadConfig() error {
 	var err error
 	baseConfig, err = config.Init(configFile)
 	if err != nil {
-		panic(fmt.Errorf("load config file %s error: %s\n", configFile, err))
+		return fmt.Errorf("load config file %s error: %w", configFile, err)
 	}
+	return nil
 }
 
-func main() {
+func startServer() *http.Server {
 	gin.SetMode(baseConfig.Mode)
 	r := gin.Default()
 
 	srv := &http.Server{
-		Addr:    baseConfig.Port,
-		Handler: router.InitRouter(r),
+		Addr:         baseConfig.Port,
+		Handler:      router.InitRouter(r),
+		ReadTimeout:  15 * time.Second,
+		WriteTimeout: 15 * time.Second,
+		IdleTimeout:  60 * time.Second,
 	}
 
 	go func() {
+		logger.Info(context.Background(), "MAIN", "Starting server on %s", baseConfig.Port)
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			panic(fmt.Errorf("Server startup failed: %v", err))
+			logger.ErrorWithMsg(context.Background(), "MAIN", "Server startup failed: %v", err)
+			os.Exit(1)
 		}
 	}()
 
-	gracefulExit(srv)
+	return srv
 }
 
 // gracefulExit 优雅退出
 func gracefulExit(srv *http.Server) {
 	exit := make(chan os.Signal, 1)
-	signal.Notify(exit, syscall.SIGINT, syscall.SIGTERM)
+	signal.Notify(exit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
 	sig := <-exit
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	logger.Info(context.Background(), "MAIN", "Received signal: %v. Shutting down server...", sig)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
-	logger.Info(ctx, "MAIN", "Received signal: %v. Shutting down server...", sig)
 
 	if err := srv.Shutdown(ctx); err != nil {
-		logger.ErrorWithMsg(ctx, "MAIN", "Server shutdown failed: %v", err)
+		logger.ErrorWithMsg(context.Background(), "MAIN", "Server shutdown failed: %v", err)
+		os.Exit(1)
 	}
-	logger.Info(ctx, "MAIN", "Server gracefully shutdown")
+
+	logger.Info(context.Background(), "MAIN", "Server gracefully shutdown")
 	fmt.Println("Server gracefully shutdown")
+}
+
+func main() {
+	if err := loadConfig(); err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to load config: %v\n", err)
+		os.Exit(1)
+	}
+
+	srv := startServer()
+
+	gracefulExit(srv)
 }
